458ee783522078b1110d43aef5fee1e7683c0c46
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-10-20 20:41:23 +0000

    s

--- DC.py
+++ DC.py
@@ -8,11 +8,11 @@ pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(100)
 try: 
   i=0
-  while i<2:
-    for dc in range(0,100):
-        pwm.ChangeDutyCycle(100-dc)
-        print(100-dc)
-        time.sleep(0.01)
-    i=i+1
+  
+  for dc in range(0,100):
+    pwm.ChangeDutyCycle(100-dc)
+    print(100-dc)
+    time.sleep(0.02)
+   
 except KeyboardInterrupt:
   GPIO.cleanup()
