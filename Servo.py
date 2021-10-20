3b7243c4a43c214955dbfdcda18c924b700833e2
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-10-20 19:25:30 +0000

    errors

--- Servo.py
+++ Servo.py
@@ -1,9 +1,10 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
 p= 5
+dc=5
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(p,GPIO.OUT)
+GPIO.setup(p, GPIO.OUT)
 T = 0.02                   # define the servo signal period
-pwm = GPIO.PWM(pin, 1/T)   # PWM instance @ 50 Hz, 1/(20 ms)
+pwm = GPIO.PWM(p, 1/T)   # PWM instance @ 50 Hz, 1/(20 ms)
 pwm.start(dc)              # Start PWM
 pwm.ChangeDutyCycle(dc)    # Change duty cycle as desired
 pwm.stop()                 # Stop PWM
